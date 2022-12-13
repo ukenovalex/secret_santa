8e6157d08c086e90b16e745439740993d104e003
ukenov <ukenov@flagstudio.ru>
2022-12-13 14:04:08 +0500

    fix logic

--- src/santa/santa.repository.ts
+++ src/santa/santa.repository.ts
@@ -2,12 +2,16 @@ import { UserModel, WishModel } from '@prisma/client';
 import { inject, injectable } from 'inversify';
 import { IPrismaService } from '../database/prisma.service.interface';
 import { HTTPError } from '../errors/http-error.class';
+import { ILogger } from '../logger/logger.interface';
 import { TYPES } from '../types';
 import { ISantaRepository } from './santa.repository.interface';
 
 @injectable()
 export class SantaRepository implements ISantaRepository {
-	constructor(@inject(TYPES.PrismaService) private prismaService: IPrismaService) {}
+	constructor(
+		@inject(TYPES.PrismaService) private prismaService: IPrismaService,
+		@inject(TYPES.ILogger) private logger: ILogger,
+	) {}
 
 	async getRandomPerson(
 		email: string,
@@ -18,14 +22,13 @@ export class SantaRepository implements ISantaRepository {
 				email: {
 					not: email,
 				},
-				donee_id: {
-					not: id,
-				},
 				isHasSanta: false,
-				isSanta: true,
+				isSanta: false,
 			},
 			include: { wishes: true },
 		});
+		// this.logger.log('firstPriority');
+		// this.logger.log(firstPriority);
 		const firstPriorityRandom = firstPriority[this.getRandomInt(firstPriority.length)];
 		if (firstPriorityRandom) {
 			await this.updateDoneeId(id, firstPriorityRandom.id);
@@ -36,14 +39,36 @@ export class SantaRepository implements ISantaRepository {
 				email: {
 					not: email,
 				},
+				donee_id: {
+					not: id,
+				},
 				isHasSanta: false,
 			},
 			include: { wishes: true },
 		});
+		// this.logger.log('secondPriority');
+		// this.logger.log(secondPriority);
 		const secondPriorityRandom = secondPriority[this.getRandomInt(secondPriority.length)];
 		if (secondPriorityRandom) {
 			await this.updateDoneeId(id, secondPriorityRandom.id);
 			return secondPriorityRandom;
+		}
+
+		const thirdPriority = await this.prismaService.client.userModel.findMany({
+			where: {
+				email: {
+					not: email,
+				},
+				isHasSanta: false,
+			},
+			include: { wishes: true },
+		});
+		// this.logger.log('thirdPriority');
+		// this.logger.log(thirdPriority);
+		const thirdPriorityRandom = secondPriority[this.getRandomInt(thirdPriority.length)];
+		if (thirdPriorityRandom) {
+			await this.updateDoneeId(id, thirdPriorityRandom.id);
+			return thirdPriorityRandom;
 		} else {
 			throw new HTTPError(500, 'нет человечков для одарения');
 		}
